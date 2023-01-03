1e57e9273921098fdbab835cc01be66250dbbe70
Nadia-website <nadia344@yandex.ru>
2023-01-03 11:16:09 +0300

    feat: solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+// module.exports = function reverse (n) {
+//     if (n === 0) return 0;
+//     if (n < 0) n = n * (-1);
+//     let arr = [];
+//     while (n > 1) {
+//         arr.push(n % 10);
+//         n = Math.trunc(n / 10);
+//     }
+//     if (Math.sign(n) === -1) {
+//         return -arr.join('');
+//     }
+//     return parseInt(arr.join(''));
+// }
 module.exports = function reverse (n) {
-  
+    if (n === 0) return 0;
+    if (n < 0) n = n * (-1);
+    return parseInt(n.toString(10).split("").reverse().join(""), 10);
 }
